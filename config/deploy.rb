59562ec13922ab49a5f99e252c9a4f936544f2d0
zhimengsun <zhimengsun@gmail.com>
2015-09-30 17:26:11 +0800

    change deployed dir for production enviroment

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:17fits/isis.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/root/sites/isis'
+set :deploy_to, '/var/www/isis'
 
 # Default value for :scm is :git
 # set :scm, :git
