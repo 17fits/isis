defe027951ad7b03e01ab49fc98f999300fd80f2
qiang.zhou <geniusfox@gmail.com>
2016-02-18 23:04:37 +0800

    delete contract and coach list

---
+++ js/data/coach.js
@@ -0,0 +1,6 @@
+var data_caoch = {
+	'tit': '金牌教练',
+	'img': 'example/top-face4.jpg',
+	'txt': '精选课程',
+	'info': '健身拥有着一批目前在国内健身行业专业而优质的私人教练团队，每年为几千名会员。'
+}
---
+++ js/js/data/store.js
@@ -0,0 +1,6 @@
+var data_store = {
+	'tit': '门店',
+	'img': 'example/top-face2.jpg',
+	'txt': '精选门店',
+	'info': '精选门店精选门店精选门店精选门店精选门店精选门店精选门店'
+}
